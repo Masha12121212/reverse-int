962aafe439a1dccb310d8fb4e5aec262dd657fac
Mariia Shaf <mashashef1212@gmail.com>
2022-01-12 09:36:59 +0200

    feat: add reverse funktion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
   
-}
+        let result = '';
+        let m = n;
+        if (n < 0) {
+            m = -1 * n;
+        }
+        let i = 0;
+        while (i < String(m).length) {
+            result = String(m)[i] + result;
+          i += 1;
+        }
+        return Number(result);
+      
+      }
